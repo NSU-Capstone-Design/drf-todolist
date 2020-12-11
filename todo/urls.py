9044d91ec23da68f75dfd7c050aeed531bc1339b
unknown <jwww0510@gmail.com>
2020-12-11 20:44:16 +0900

    test

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from todo.views import *
 
 urlpatterns = [
+    path('category/get/', get_category),
     path('category/', add_category),
     path('category/<int:pk>', del_category),
     path('todo/', add_todo),
--- todo/views.py
+++ todo/views.py
@@ -8,24 +8,23 @@ from todo.serializers import CategorySerializer, TodoSerializer
 from core.serializers import TodoListSerializer
 from rest_framework.views import APIView
 
+@api_view(['GET'])
+def get_category(request):
+    token = request.data.get("token")
+    category = Category.objects.filter(user=token)
+    serializer = CategorySerializer(category, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def add_category(request):
-    if request.method == 'GET':
-        token = request.data.get("token")
-        category = Category.objects.filter(user=token)
-        serializer = CategorySerializer(category, many=True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-
-    elif request.method == 'POST':
-        token = request.data.get("token")
-        user = authorize(token)
-        if not user:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        Category.objects.create(title=request.data.get("title"), user=user)
-        return Response(data={
-            "content": "success",
-        }, status = status.HTTP_200_OK)
+    token = request.data.get("token")
+    user = authorize(token)
+    if not user:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    Category.objects.create(title=request.data.get("title"), user=user)
+    return Response(data={
+        "content": "success",
+    }, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'DELETE'])
@@ -37,7 +36,7 @@ def del_category(request, pk):
 
     if request.method == 'GET':
         category = CategorySerializer(category)
-        return Response(category.data, status = status.HTTP_200_OK)
+        return Response(category.data, status=status.HTTP_200_OK)
 
     elif request.method == 'DELETE':
         category.delete()
@@ -60,7 +59,7 @@ def add_todo(request):
     user.categories.get(title=category).todos.create(title=title, content=content)
     return Response(data={
         "content": "success",
-    }, status = status.HTTP_200_OK)
+    }, status=status.HTTP_200_OK)
 
 
 '''------------------LDK------------------------------'''
@@ -71,8 +70,8 @@ def add_todo(request):
 def todo_list(request):
     if request.method == 'GET':
         queryset = Todo.objects.all()
-        serializer = TodoSerializer(queryset, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         listPK = request.data.get("listPK")
         todo_list = Todo.objects.get(pk=listPK)
@@ -83,20 +82,25 @@ def todo_list(request):
 # 리스트 생성
 @api_view(["POST"])
 def addTodo(request):
+    token = request.data.get("token")
     categoryPK = request.data.get("categoryPK")
     title = request.data.get("title")
     content = request.data.get('content')
 
     category = Category.objects.get(pk = categoryPK)
 
+    user = authorize(token)
+    if not user:
+        return Response(status = status.HTTP_401_UNAUTHORIZED)
+
     if not title and not content:
         return Response(status = status.HTTP_400_BAD_REQUEST)
     elif not title:
-        category.todos.create(title = "ListTitle", content = content)
+        category.todos.create(title = "<Empty_title>", content = content)
         return Response(status = status.HTTP_200_OK)
     elif not content:
         category.todos.create(title = title, content = '')
         return Response(status = status.HTTP_200_OK)
+
     category.todos.create(title = title, content = content)
     return Response(status = status.HTTP_200_OK)
-
