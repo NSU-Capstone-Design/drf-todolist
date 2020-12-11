70d11ab34102bc546314360e19ab1b42c3f83def
JeoungSulMo <tjdah0853@gmail.com>
2020-12-11 14:59:40 +0900

    id 추가

--- todo/serializers.py
+++ todo/serializers.py
@@ -6,10 +6,10 @@ from todo.models import Todo, Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['title', 'created_at', 'user']
+        fields = ['id', 'title', 'created_at', 'user']
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['title', 'content', 'created_at', 'category', 'checked']
+        fields = ['id', 'title', 'content', 'created_at', 'category', 'checked']
