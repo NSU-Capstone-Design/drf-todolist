bc7e98b52d3ad89cf862ed662cac0090cc47b2df
Ubuntu <ubuntu@ip-172-31-20-181.ec2.internal>
2020-12-12 08:59:46 +0000

    배포완료

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -110,3 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
